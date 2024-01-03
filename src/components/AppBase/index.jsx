7d79e1f0b7b7b2e8b88c7003a33fd8c91c6b17e6
Dayana Ilieva <dayana.ilieva@1forfit.com>
2024-01-03 13:07:26 +0200

    Fix failing tests and separate selector working with state

--- src/components/AppBase/index.jsx
+++ src/components/AppBase/index.jsx
@@ -2,12 +2,11 @@
 import { object } from 'prop-types';
 import { useAppDispatch } from '@/hooks';
 
-import { setConfig, setDevToolsStatus } from '@/store/slices/config';
+import { setConfig } from '@/store/slices/config';
 import { setMeta } from '@/store/slices/meta';
 
 import { LayoutBase, LayoutHead, LayoutFoot } from '@/components/Layout';
 import { StreamBase } from '@/components/Stream';
-import { DevTools } from '@/components/DevTools';
 
 export const AppBase = ({ config = {} }) => {
   const dispatch = useAppDispatch();
@@ -15,18 +14,13 @@ export const AppBase = ({ config = {} }) => {
   // Set defaults
   dispatch(setMeta(config.meta));
   dispatch(setConfig(config.app));
-  dispatch(setDevToolsStatus(false)); // DEV Note: set this in a global plugin process.env.NODE_ENV === 'development'
 
   return (
-    <>
-      <DevTools />
-
-      <LayoutBase
-        head={ <LayoutHead /> }
-        stream={ <StreamBase /> }
-        foot={ <LayoutFoot /> }
-      />
-    </>
+    <LayoutBase
+      head={ <LayoutHead /> }
+      stream={ <StreamBase /> }
+      foot={ <LayoutFoot /> }
+    />
   );
 };
 
